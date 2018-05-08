02f8441fd25e6740cb53a557e490d031a22768a6
Yakima <cleveryun@163.com>
2018-05-08 10:28:54 +0800

    add function getCookie

--- docs/SUMMARY.md
+++ docs/SUMMARY.md
@@ -1,6 +1,7 @@
 # TOC
 
 * [Preface](README.md)
+* [getCookie](./getCookie.md)
 * [hasValue](./hasValue.md)
 * [setCookie](./setCookie.md)
 * [validateIdCardNo](./validateIdCardNo.md)
---
+++ getCookie.js
@@ -0,0 +1,22 @@
+/**
+ * Get value of the cookie item of specified name
+ * @param name name of the cookie item
+ * @returns {string} value of the cookie item
+ */
+function getCookie (name) {
+  if (document.cookie.length > 0) {
+    let start = document.cookie.indexOf(name + '=')
+    if (start !== -1) {
+      start = start + name.length + 1
+      let end = document.cookie.indexOf(';', start)
+      if (end === -1) {
+        end = document.cookie.length
+      }
+      return window.decodeURI(document.cookie.substring(start, end))
+    }
+    return ''
+  }
+  return ''
+}
+
+export default getCookie
