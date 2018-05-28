a63ca51c6587b02cdbadcbeb9c5e7b9873fe5616
Yakima <cleveryun@163.com>
2018-05-28 16:36:43 +0800

    add example code for functions fillLeft, getString, toDouble

--- getString.js
+++ getString.js
@@ -4,6 +4,14 @@
  * Transform value to string format
  * @param val {any} any value you want to transfer to string format
  * @returns {string} value in string format
+ *
+ * @example
+ * ```javascript
+ * console.log(getString(1)) // '1'
+ * console.log(getString(0)) // '0'
+ * console.log(getString(null)) // ''
+ * console.log(getString(undefined)) // ''
+ * ```
  */
 function getString (val: any): string {
   return val === 0 ? '0' : (val ? ('' + val) : '')
--- toDouble.js
+++ toDouble.js
@@ -4,9 +4,15 @@ import fillLeft from './fillLeft'
 
 /**
  * Make a number less than 10 to be prefixed with an '0'
- * - the same as `fillLeft(m, 2, '0')`
+ * - the same as `fillLeft(val, 2, '0')`
  * @param num {string|number} a number, or number in string format (number should be integer)
  * @returns {string} string after prefixed with '0' is less than 10
+ *
+ * @example
+ * ```javascript
+ * console.log(toDouble('1')) // '01'
+ * console.log(toDouble('11')) // '11'
+ * ```
  */
 function toDouble (num: string | number): string {
   return fillLeft(num, 2, '0')
