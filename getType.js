c7e73caa227f7a6899b3e1c2d45b5f6f14b38f4b
Yakima <cleveryun@163.com>
2018-05-28 15:03:49 +0800

    add example doc for functions getType and hasValue

--- getType.js
+++ getType.js
@@ -4,6 +4,21 @@
  * Get the type of a variable
  * @param val {any} the variable
  * @returns {string} 'array', 'object', 'function', 'null', 'undefined', 'string', 'number', 'boolean', 'date', 'regexp' and etc.
+ *
+ * @example
+ *
+ * ```javascript
+ * console.log(getType({}) // 'object'
+ * console.log(getType([]) // 'array'
+ * console.log(getType(() => {})) // 'function'
+ * console.log(getType(null)) // 'null'
+ * console.log(getType(undefined)) // 'undefined'
+ * console.log(getType('')) // 'string'
+ * console.log(getType(123)) // 'number'
+ * console.log(getType(true)) // 'boolean'
+ * console.log(getType(new Date())) // 'date'
+ * console.log(getType(/^[0-9]{3}/)) // 'regexp'
+ * ```
  */
 function getType (val: any): string {
   return ({}).toString.call(val).slice(8, -1).toLowerCase()
--- hasValue.js
+++ hasValue.js
@@ -6,6 +6,18 @@
  * - number 0 is regarded as valuable
  * @param val variable
  * @returns {boolean} whether a variable is valuable
+ *
+ * @example
+ *
+ * ```javascript
+ * console.log(hasValue('')) // false
+ * console.log(hasValue(null) // false
+ * console.log(hasValue('null') // false
+ * console.log(hasValue(undefined) // false
+ * console.log(hasValue('undefined') // false
+ * console.log(hasValue(0) // true
+ * console.log(hasValue('none') // true
+ * ```
  */
 function hasValue (val: any): boolean {
   return val !== '' && val !== null && val !== undefined && val !== 'undefined' && val !== 'null' && val !== 'undefined'
