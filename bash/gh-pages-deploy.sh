c66455ed2e86cd2e6253f06539f3766389650806
yakima <cleveryun@163.com>
2024-01-29 10:41:50 +0800

    修复因为git init默认分支为main导致git push -f https://github.com/Yakima-Teng/utils-daily.git master:gh-pages命令失败的问题

--- bash/gh-pages-deploy.sh
+++ bash/gh-pages-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd dist
-git init
+git init --initial-branch=master
 git add -A
 git commit -m 'update github pages'
 git push -f https://github.com/Yakima-Teng/utils-daily.git master:gh-pages
